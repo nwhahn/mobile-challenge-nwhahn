22a2a142ef8448b62513aee3aa4b6ac5d7ddab18
Nate Hahn <nathan@nwhahn.com>
2021-09-06 14:16:10 -0400

    More detail in LandingListItem

--- components/cards/LandingListItem.tsx
+++ components/cards/LandingListItem.tsx
@@ -7,6 +7,7 @@ import {IconButton} from '../buttons';
 import {useAppDispatch, useAppSelector} from '../../redux/hooks';
 import {getLandingIsFavorited} from '../../redux/selectors';
 import ActionTypes from '../../redux/actionTypes';
+import theme from '../../theme';
 interface LandingListItemProps {
   data: MeteoriteLanding;
 }
@@ -17,20 +18,44 @@ const TouchableWrapper = styled.TouchableOpacity`
   border-radius: 8px;
   flex-direction: row;
 `;
-
+const FieldKey = styled.Text`
+  color: ${theme.colors.text}40;
+  margin-right: 8px;
+`;
+const FieldValue = styled.Text`
+  color: ${theme.colors.text};
+`;
 const Header = styled.Text`
   color: ${({color}) => color};
-  font-size: 20;
+  font-size: 20px;
 `;
 const Info = styled.View`
   flex: 3;
 `;
+const Line = styled.View`
+  height: 1px;
+  background-color: ${theme.colors.text};
+  width: 100%;
+  margin-bottom: 4px;
+`;
 
 const Actions = styled.View`
   flex: 1;
-  justify-content: flex-end;
+  justify-content: center;
+  align-items: flex-end;
 `;
-const LandingListItem = ({data: {name, id}}: LandingListItemProps) => {
+const DataWrapper = styled.View`
+  padding-top: 4px;
+`;
+
+const DataRow = styled.View`
+  flex: 1;
+  flex-direction: row;
+  padding-bottom: 4px;
+`;
+const LandingListItem = ({
+  data: {name, id, year, mass},
+}: LandingListItemProps) => {
   const {colors} = useTheme();
   const {card, text} = colors;
   const isFavorited = useAppSelector(state => getLandingIsFavorited(state, id));
@@ -39,6 +64,17 @@ const LandingListItem = ({data: {name, id}}: LandingListItemProps) => {
     <TouchableWrapper color={card}>
       <Info>
         <Header color={text}>{name}</Header>
+        <Line />
+        <DataWrapper>
+          <DataRow>
+            <FieldKey>Year:</FieldKey>
+            <FieldValue>{new Date(year).getFullYear()}</FieldValue>
+          </DataRow>
+          <DataRow>
+            <FieldKey>Mass:</FieldKey>
+            <FieldValue>{mass}</FieldValue>
+          </DataRow>
+        </DataWrapper>
       </Info>
       <Actions>
         <IconButton
